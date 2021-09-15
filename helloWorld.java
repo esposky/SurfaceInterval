3f61a1fa1690c7e2072f87ac196b73948e401505
SJamesConcep <sconcepcion@usf.edu>
2021-09-14 21:05:02 -0400

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -3,3 +3,9 @@ class HelloWorld {
         System.out.println("Hello, My World!");
     }
 }
+
+class HelloWord_2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, To This World!");
+    }
+}
